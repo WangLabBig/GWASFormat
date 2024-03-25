465dc800c5088c68d8b72111949bdb209bba579d
big_xutingfeng <13984376+big-xutingfeng@user.noreply.gitee.com>
2024-03-25 21:10:05 +0800

    fixbugs

--- scripts/versionConvert.py
+++ scripts/versionConvert.py
@@ -369,9 +369,10 @@ if __name__ == "__main__":
                         line.append(new_pos)
                 ss = outputDelimter.join(line)
             except:
-                raise ValueError(
-                    f"Wrong with line: {line}\n while the output of liftover is {lifter_res}"
+                sys.stderr.write(
+                    f"Error with line: {line}\n while the output of liftover is {lifter_res}"
                 )
+                raise
 
         line_idx += 1
         if line_need_skip and not keep_unmapped:
